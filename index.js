49d3769a70e3ced4d57e3c8c6dcd643a2ea61945
Sierra Curtis <sierracurtis91@gmail.com>
2019-11-14 14:33:41 -0500

    finshed the first 21 problems

--- index.js
+++ index.js
@@ -74,7 +74,24 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons
+  }
+  drive(distance){
+    this.odometer = this.odometer + distance,
+    this.tank = this.tank - distance / this.milesPerGallon;
+    if(distance > this.tank * this.milesPerGallon){
+      this.odometer = this.odometer + this.tank * this.milesPerGallon;
+      this.tank = 0
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
 }
 
 /*
