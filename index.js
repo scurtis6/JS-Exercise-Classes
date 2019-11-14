2b8ef35bb34f0f091c8854c688ba61ddc748eadd
Sierra Curtis <sierracurtis91@gmail.com>
2019-11-14 14:22:06 -0500

    finished the first 10

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name,
+    this.age = age,
+    this.stomach = []
+  }
+  eat(somefood){
+    if(this.stomach.length < 10){
+      this.stomach.push(somefood)
+    }
+  }
+  poop(){
+    this.stomach =[]
+  }
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
 }
 
 /*
